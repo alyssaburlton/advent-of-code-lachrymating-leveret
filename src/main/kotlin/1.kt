adb717e13307bae3a4229da5dc079485d92eb017
Alyssa <alyssa.burlton@gmail.com>
2022-12-01 05:08:56 +0000

    bit less gross

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -1,12 +1,12 @@
 class Day1 : Solver {
     override val day = 1
 
-    private val input = readGroupedList("1")
+    private val input = readIntegerGroupedList("1")
 
-    override fun partA() = input.maxBy { it.sum() }.sum()
+    override fun partA() = input.maxOf { it.sum() }
 
     override fun partB(): Int {
         val sorted = input.sortedByDescending { it.sum() }
-        return sorted[0].sum() + sorted[1].sum() + sorted[2].sum()
+        return sorted.subList(0, 3).sumOf { it.sum() }
     }
 }
--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -3,9 +3,11 @@ import java.io.File
 fun readStringList(filename: String) =
     File("src/main/resources/$filename.txt").readLines()
 
-fun readGroupedList(filename: String): List<List<Int>> =
+fun readGroupedList(filename: String): List<List<String>> =
     File("src/main/resources/$filename.txt").readText().trim().split("\n\n").map { it.split("\n") }
-        .map { list -> list.map { it.toInt() } }
+
+fun readIntegerGroupedList(filename: String) =
+    readGroupedList(filename).map { group -> group.map { it.toInt() } }
 
 fun readIntegerList(filename: String) =
     readStringList(filename).map { it.toInt() }
