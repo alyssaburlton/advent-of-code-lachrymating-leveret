e1990d9b64e6779d3a19d6e04fed17307f803c0d
Alyssa <alyssa.burlton@gmail.com>
2022-12-01 05:06:27 +0000

    day 1 gross :D

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -3,5 +3,9 @@ import java.io.File
 fun readStringList(filename: String) =
     File("src/main/resources/$filename.txt").readLines()
 
+fun readGroupedList(filename: String): List<List<Int>> =
+    File("src/main/resources/$filename.txt").readText().trim().split("\n\n").map { it.split("\n") }
+        .map { list -> list.map { it.toInt() } }
+
 fun readIntegerList(filename: String) =
     readStringList(filename).map { it.toInt() }
