eac958f7cee5715a59ffd8d0d4ecd1ec28dd3d59
Alyssa <alyssa.burlton@gmail.com>
2022-12-04 05:16:04 +0000

    some tidying of day 3

--- src/main/kotlin/3.kt
+++ src/main/kotlin/3.kt
@@ -8,6 +8,12 @@ class Day3 : Solver {
 
     override fun partB() = input.chunked(3).map(::findGroupItem).sum()
 
+    private fun findItemInBothCompartments(rucksack: String): Int {
+        val (firstHalf, secondHalf) = rucksack.chunked(rucksack.length / 2).map(::rucksackToLetterSet)
+        val intersection = firstHalf.intersect(secondHalf)
+        return getScore(intersection)
+    }
+
     private fun findGroupItem(group: List<String>): Int {
         val sets = group.map(::rucksackToLetterSet)
         val intersection = sets.fold(sets.first()) { acc, strings ->
@@ -17,23 +23,10 @@ class Day3 : Solver {
         return getScore(intersection)
     }
 
-    private fun findItemInBothCompartments(rucksack: String): Int {
-        val length = rucksack.length
-        val firstHalf = rucksackToLetterSet(rucksack.substring(0, length / 2))
-        val secondHalf = rucksackToLetterSet(rucksack.substring(length / 2, length))
-
-        val intersection = firstHalf.intersect(secondHalf)
-        return getScore(intersection)
-    }
-
-    private fun getScore(rucksackIntersection: Set<String>): Int {
-        if (rucksackIntersection.size > 1) {
-            throw Error("Ooops")
-        }
-
-        val character = rucksackIntersection.first().toCharArray()[0]
+    private fun getScore(rucksackIntersection: Set<Char>): Int {
+        val character = rucksackIntersection.first()
         return itemsByPriority.indexOf(character) + 1
     }
 
-    private fun rucksackToLetterSet(rucksack: String) = rucksack.split("").filter { it.isNotEmpty() }.toSet()
+    private fun rucksackToLetterSet(rucksack: String) = rucksack.toCharArray().toSet()
 }
