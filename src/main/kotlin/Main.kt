76beca76bc05acb129127007703f746eb6c2526a
Alyssa <alyssa.burlton@gmail.com>
2022-12-08 10:22:33 +0000

    ANSI formatted timings :D

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,15 +9,26 @@ val solvers = listOf(
     Day8()
 )
 
+const val RESET_COLOR = "\u001b[0m"
+const val ITALIC = "\u001b[3m"
+const val RED = "\u001b[31m"
+const val GREEN = "\u001b[32m"
+const val AMBER = "\u001b[33m"
+
+
 fun main() {
     solvers.forEach { solver ->
-        val timerA = DurationTimer()
-        val resultA = solver.partA()
-        println("(${timerA.duration()}ms) ${solver.day}A: $resultA")
-
-        val timerB = DurationTimer()
-        val resultB = solver.partB()
-        println("(${timerB.duration()}ms) ${solver.day}B: $resultB")
+        timedSolution(solver, "A") { solver.partA() }
+        timedSolution(solver, "B") { solver.partB() }
         println()
     }
 }
+
+private fun timedSolution(solver: Solver, part: String, solverFn: Solver.() -> Any?) {
+    val timer = DurationTimer()
+    val result = solverFn(solver)
+    val duration = timer.duration()
+    println("$ITALIC${colour(duration)}(${duration}ms)$RESET_COLOR ${solver.day}$part: $result")
+}
+
+private fun colour(duration: Long): String = if (duration < 1000) GREEN else if (duration < 5000) AMBER else RED
