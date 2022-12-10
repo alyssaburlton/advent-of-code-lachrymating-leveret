0b18439284f3f3187e1282eadc75f1813b159bef
Alyssa <alyssa.burlton@gmail.com>
2022-12-10 18:55:27 +0000

    start neatening up

--- src/main/kotlin/10.kt
+++ src/main/kotlin/10.kt
@@ -1,120 +1,62 @@
+data class CpuState(
+    val queuedInstructions: List<Int?>,
+    val pendingInstructions: List<Int?>,
+    val register: Int,
+    val cycle: Int,
+    val processingAdd: Boolean,
+    val pixels: List<String>
+)
+
+const val PROGRAM_LENGTH = 240
+
 class Day10 : Solver {
     override val day = 10
 
     private val input = readStringList("10")
 
+    private fun initialCpuState() = CpuState(emptyList(), input.map(::parseInstruction), 1, 1, false, emptyList())
+
     override fun partA(): Any {
-        var register = 1
-        var sum = 0
-
-        var startedAddX = false
-
-        val pendingInstructions = mutableListOf<Int?>()
-
-        val programLength = 280
-
-        val interestingCycles = (20..programLength step 40)
-
-        (0 until programLength).forEach { ix ->
-            val instruction = if (ix < input.size) input[ix] else null
-
-            val cycle = ix + 1
-            // println("Starting cycle $cycle, register = $register")
-
-            // Start
-            pendingInstructions.add(parseInstruction(instruction))
-
-
-
-            // During
-            // println("During cycle $cycle, instructions = $pendingInstructions")
-
-            if (interestingCycles.contains(cycle)) {
-                sum += (cycle * register)
-                println("Cycle $cycle: $register = ${cycle * register}. Sum = $sum")
-            }
-
-            // End
-            val pendingInstruction = pendingInstructions[0]
-            if (pendingInstruction == null) {
-                pendingInstructions.removeAt(0)
-            } else if (startedAddX) {
-                register += pendingInstruction
-                pendingInstructions.removeAt(0)
-                startedAddX = false
-            } else {
-                startedAddX = true
-            }
-
-
-            // println("Finished cycle $cycle, register = $register")
-        }
-        return ""
+        val cpuStates =
+            (0 until PROGRAM_LENGTH).runningFold(initialCpuState()) { cpuState, _ -> cpuState.doTick() }
+        return cpuStates.filter { (20..PROGRAM_LENGTH step 40).contains(it.cycle) }.sumOf { it.cycle * it.register }
     }
 
-    private fun parseInstruction(instruction: String?): Int? {
-        if (instruction != null && instruction.startsWith("addx")) {
-            return  instruction.split(" ")[1].toInt()
+    override fun partB() =
+        (0 until PROGRAM_LENGTH).fold(initialCpuState()) { cpuState, _ -> cpuState.doTick() }.getPixelImage()
+
+    private fun parseInstruction(instruction: String) =
+        if (instruction.startsWith("addx")) {
+            instruction.split(" ")[1].toInt()
+        } else null
+
+    private fun CpuState.getPixelImage() = pixels.chunked(40).joinToString("\n", "\n") { it.joinToString("") }
+
+    private fun CpuState.doTick(): CpuState {
+        val newInstruction = pendingInstructions.firstOrNull()
+
+        val queuedInstruction = queuedInstructions.firstOrNull()
+        val newProcessingAdd = queuedInstruction != null && !processingAdd
+
+        val remainingQueuedInstructions = if (!newProcessingAdd) queuedInstructions.drop(1) else queuedInstructions
+
+        val newRegister = if (newProcessingAdd) {
+            register + queuedInstruction!!
+        } else register
+
+        val newPixel = if ((register - 1..register + 1).contains((cycle - 1) % 40)) {
+            "#"
+        } else {
+            "."
         }
 
-        return null
-    }
-
-    override fun partB(): Any {
-        var register = 1
-        var sum = 0
-
-        var startedAddX = false
-
-        val pendingInstructions = mutableListOf<Int?>()
-        val pixels = mutableListOf<String>()
-
-        val programLength = 240
-
-        val interestingCycles = (20..programLength step 40)
-
-        (0 until programLength).forEach { ix ->
-            val instruction = if (ix < input.size) input[ix] else null
-
-            val cycle = ix + 1
-            // println("Starting cycle $cycle, register = $register")
-
-            // Start
-            pendingInstructions.add(parseInstruction(instruction))
-
-
-
-            // During
-            // draw sprite
-
-            if ((register-1..register+1).contains(ix % 40)) {
-                pixels.add("#")
-            } else {
-                pixels.add(".")
-            }
-
-            if (interestingCycles.contains(cycle)) {
-                sum += (cycle * register)
-                println("Cycle $cycle: $register = ${cycle * register}. Sum = $sum")
-            }
-
-            // End
-            val pendingInstruction = pendingInstructions[0]
-            if (pendingInstruction == null) {
-                pendingInstructions.removeAt(0)
-            } else if (startedAddX) {
-                register += pendingInstruction
-                pendingInstructions.removeAt(0)
-                startedAddX = false
-            } else {
-                startedAddX = true
-            }
-
-
-            // println("Finished cycle $cycle, register = $register")
-        }
-
-        pixels.chunked(40).map { it.joinToString("") }.forEach { println(it) }
-        return ""
+        return CpuState(
+            remainingQueuedInstructions + newInstruction,
+            pendingInstructions.drop(1),
+            newRegister,
+            cycle + 1,
+            newProcessingAdd,
+            pixels + newPixel
+        )
     }
 }
