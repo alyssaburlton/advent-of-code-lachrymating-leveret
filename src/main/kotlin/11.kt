0b0bd1bdb713735fbfcb76cbe73f7edd43d807ef
Alyssa <alyssa.burlton@gmail.com>
2022-12-11 06:45:45 +0000

    some tidying

--- src/main/kotlin/11.kt
+++ src/main/kotlin/11.kt
@@ -4,58 +4,11 @@ data class MonkeyTest(val divisor: Int, val monkeyIfTrue: Int, val monkeyIfFalse
 class Day11 : Solver {
     override val day = 11
 
-    private val input: List<List<String>> = readGroupedList("11e")
+    private val input: List<List<String>> = readGroupedList("11")
 
-    override fun partA(): Any {
-        var currentMonkeys = parseMonkeys()
-        val monkeyIndexes = currentMonkeys.keys.sorted()
-        val itemsInspected = mutableMapOf<Int, Int>()
+    override fun partA() = calculateMonkeyBusiness(20, true)
 
-        (1..20).forEach { _ ->
-            monkeyIndexes.forEach { monkeyIx ->
-                val newItemLocations = mutableMapOf<Int, List<Long>>()
-                println("Monkey $monkeyIx:")
-                val monkey = currentMonkeys.getValue(monkeyIx)
-                val inspecting = itemsInspected.getOrDefault(monkeyIx, 0) + monkey.items.size
-                itemsInspected[monkeyIx] = inspecting
-
-                monkey.items.forEach { item ->
-                    println("   Monkey inspects an item with a worry level of $item")
-                    val intermediateValue = monkey.operation(item)
-                    println("       After worry level: $intermediateValue")
-                    val newValue = intermediateValue / 3
-                    println("       After boredom: $newValue")
-                    val newMonkey =
-                        if (newValue % monkey.test.divisor == 0L) monkey.test.monkeyIfTrue else monkey.test.monkeyIfFalse
-
-                    println("       Thrown to monkey $newMonkey")
-
-                    val items = newItemLocations.getOrDefault(newMonkey, emptyList())
-                    newItemLocations[newMonkey] = items + newValue
-                }
-
-                println(newItemLocations)
-                currentMonkeys = currentMonkeys.mapValues { (newMonkeyIx, newMonkey) ->
-                    when (newMonkeyIx) {
-                        monkeyIx -> newMonkey.copy(items = emptyList())
-                        else -> newMonkey.copy(
-                            items = newMonkey.items + newItemLocations.getOrDefault(
-                                newMonkeyIx,
-                                emptyList()
-                            )
-                        )
-                    }
-                }
-            }
-
-            println(currentMonkeys.mapValues { it.value.items })
-        }
-
-        println(itemsInspected)
-        val result = itemsInspected.values.sortedDescending()
-        println(result)
-        return result[0] * result[1]
-    }
+    override fun partB() = calculateMonkeyBusiness(10000, false)
 
     private fun parseMonkeys(): Map<Int, Monkey> {
         return input
@@ -81,7 +34,7 @@ class Day11 : Solver {
         return Monkey(startingItems, operation, MonkeyTest(testDivisor, monkeyIfTrue, monkeyIfFalse))
     }
 
-    override fun partB(): Any {
+    private fun calculateMonkeyBusiness(rounds: Int, divideByThree: Boolean): Long {
         var currentMonkeys = parseMonkeys()
         val monkeyIndexes = currentMonkeys.keys.sorted()
         val itemsInspected = mutableMapOf<Int, Long>()
@@ -89,28 +42,23 @@ class Day11 : Solver {
         val uniqueDivisors = currentMonkeys.values.map { it.test.divisor }.distinct()
         val modBy = uniqueDivisors.distinct().product()
 
-        (1..10000).forEach { _ ->
+        (1..rounds).forEach { _ ->
             monkeyIndexes.forEach { monkeyIx ->
                 val newItemLocations = mutableMapOf<Int, List<Long>>()
-                // println("Monkey $monkeyIx:")
                 val monkey = currentMonkeys.getValue(monkeyIx)
                 val inspecting = itemsInspected.getOrDefault(monkeyIx, 0) + monkey.items.size
                 itemsInspected[monkeyIx] = inspecting
 
                 monkey.items.forEach { item ->
-                    // println("   Monkey inspects an item with a worry level of $item")
-                    val newValue = monkey.operation(item) % modBy
-                    // println("       After boredom: $newValue")
+                    val newValue = if (divideByThree) monkey.operation(item) / 3 else monkey.operation(item) % modBy
+
                     val newMonkey =
                         if (newValue % monkey.test.divisor == 0L) monkey.test.monkeyIfTrue else monkey.test.monkeyIfFalse
-
-                    // println("       Thrown to monkey $newMonkey")
 
                     val items = newItemLocations.getOrDefault(newMonkey, emptyList())
                     newItemLocations[newMonkey] = items + newValue
                 }
 
-                // println(newItemLocations)
                 currentMonkeys = currentMonkeys.mapValues { (newMonkeyIx, newMonkey) ->
                     when (newMonkeyIx) {
                         monkeyIx -> newMonkey.copy(items = emptyList())
@@ -123,13 +71,9 @@ class Day11 : Solver {
                     }
                 }
             }
-
-            // println(currentMonkeys.mapValues { it.value.items })
         }
 
-        println(itemsInspected)
         val result = itemsInspected.values.sortedDescending()
-        println(result)
         return result[0] * result[1]
     }
 }
