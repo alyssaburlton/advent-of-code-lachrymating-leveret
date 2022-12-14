0e2ed59d9dc8831fd0b377f3dbc3d783949c8f74
Alyssa <alyssa.burlton@gmail.com>
2022-12-14 06:51:19 +0000

    No mutation, now Part B is ~9s

--- src/main/kotlin/14.kt
+++ src/main/kotlin/14.kt
@@ -9,69 +9,52 @@ class Day14 : Solver {
     override fun partB() = dropAllSand(true)
 
     private fun dropAllSand(withFloor: Boolean): Int {
-        val grid = parseGrid(withFloor)
-        val yMax = grid.keys.maxOf { it.y }
+        val rockPoints = getRockPoints()
+        val lowestRock = rockPoints.maxOf { it.y }
+        val yMax = if (withFloor) lowestRock + 2 else lowestRock
+        val isRockPoint = { pt: Point -> rockPoints.contains(pt) || (withFloor && pt.y == yMax) }
 
-        var stillMoving = dropSand(grid, yMax)
-        while (stillMoving) {
-            stillMoving = dropSand(grid, yMax)
-        }
-
-        return grid.values.count { it == "o" }
+        val result = dropSandRecursive(yMax, isRockPoint)
+        return result.size
     }
 
-    private fun dropSand(grid: MutableMap<Point, String>, yMax: Int): Boolean {
-        val newPoint = getNextMove(grid, yMax)
-        if (newPoint != null) {
-            grid[newPoint] = "o"
-        }
-
-        return newPoint != null
+    private tailrec fun dropSandRecursive(
+        yMax: Int,
+        isRockPoint: (Point) -> Boolean,
+        sandPoints: Set<Point> = emptySet()
+    ): Set<Point> {
+        val newPoint = getNextMove(sandPoints, yMax, isRockPoint) ?: return sandPoints
+        return dropSandRecursive(yMax, isRockPoint, sandPoints + newPoint)
     }
 
-    private fun getNextMove(grid: Map<Point, String>, yMax: Int, point: Point = dropPoint): Point? {
-        if (grid[dropPoint] == "o") {
+    private fun getNextMove(
+        sandPoints: Set<Point> = emptySet(),
+        yMax: Int,
+        isRockPoint: (Point) -> Boolean,
+        point: Point = dropPoint
+    ): Point? {
+        if (sandPoints.contains(dropPoint)) {
             return null
         }
 
         val preferredPoints =
             listOf(Point(point.x, point.y + 1), Point(point.x - 1, point.y + 1), Point(point.x + 1, point.y + 1), point)
 
-        val newPoint = preferredPoints.first { grid.getOrDefault(it, ".") == "." }
+        val newPoint =
+            preferredPoints.first { pt -> !isRockPoint(pt) && !sandPoints.contains(pt) }
         return if (newPoint.y > yMax) {
             null
         } else if (newPoint == point) {
             point
         } else {
-            getNextMove(grid, yMax, newPoint)
+            getNextMove(sandPoints, yMax, isRockPoint, newPoint)
         }
     }
 
-    private fun parseGrid(withFloor: Boolean): MutableMap<Point, String> {
-        val rockPoints: Set<Point> = input.flatMap { inputLine ->
-            val pointPairs = inputLine.split(" -> ").map(::parsePoint).windowed(2)
-            pointPairs.flatMap { getLine(it[0], it[1]) }
-        }.toSet()
-
-        val xMin = rockPoints.minOf { it.x }
-        val xMax = rockPoints.maxOf { it.x }
-        val yMax = rockPoints.maxOf { it.y }
-
-        val xStart = if (withFloor) xMin - 200 else xMin
-        val xEnd = if (withFloor) xMax + 200 else xMax
-        val yEnd = if (withFloor) yMax + 2 else yMax
-
-        val map = mutableMapOf<Point, String>()
-
-        for (x in (xStart..xEnd)) {
-            for (y in 0..yEnd) {
-                map[Point(x, y)] =
-                    if (rockPoints.contains(Point(x, y))) "#" else if (withFloor && y == yEnd) "#" else "."
-            }
-        }
-
-        return map
-    }
+    private fun getRockPoints(): Set<Point> = input.flatMap { inputLine ->
+        val pointPairs = inputLine.split(" -> ").map(::parsePoint).windowed(2)
+        pointPairs.flatMap { getLine(it[0], it[1]) }
+    }.toSet()
 
     private fun parsePoint(pointStr: String): Point {
         val split = pointStr.split(",")
