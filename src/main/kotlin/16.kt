4017c2a6e6a0a48cc4285342325ead1fd962fa55
Alyssa <alyssa.burlton@gmail.com>
2022-12-16 14:09:20 +0000

    No hardcoding, and runs fast without needing crazy RAM

--- src/main/kotlin/16.kt
+++ src/main/kotlin/16.kt
@@ -2,7 +2,7 @@ data class Valve(val room: String, val flowRate: Int)
 data class MoveState(
     val myValve: PersonState,
     val elephantValve: PersonState?,
-    val releasedValves: List<Pair<Int, Valve>>
+    val releasedValves: Set<Pair<Int, Valve>>
 )
 
 data class PersonState(val currentValve: Valve, val prevValves: Set<Valve>)
@@ -14,107 +14,90 @@ class Day16 : Solver {
     private val input = readStringList("16").map(::parseValves)
     private val valvesMap = getRoutedValvesMap(input)
     private val releasableValves = valvesMap.keys.filter { it.flowRate > 0 }
+    private val startingValve = valvesMap.keys.first { it.room == "AA" }
 
     override fun partA(): Any {
-        val startingValve = valvesMap.keys.first { it.room == "AA" }
-        val startingPerson = PersonState(startingValve, emptySet())
-        val initialState = MoveState(startingPerson, null, emptyList())
-        // return runSimulation(initialState, 30)
-        return -1
+        val initialState = MoveState(initialPersonState(), null, emptySet())
+        return runSimulation(initialState, 30)
     }
 
     override fun partB(): Any {
-        val startingValve = valvesMap.keys.first { it.room == "AA" }
-        val startingPerson = PersonState(startingValve, emptySet())
-        val initialState = MoveState(startingPerson, startingPerson, emptyList())
+        val initialState = MoveState(initialPersonState(), initialPersonState(), emptySet())
         return runSimulation(initialState, 26)
     }
 
-    private fun countTotalReleased(move: MoveState): Int {
-        return move.releasedValves.sumOf {
-            it.first * it.second.flowRate
-        }
+    private fun initialPersonState() = PersonState(startingValve, emptySet())
+
+    private fun countTotalReleased(move: MoveState) = move.releasedValves.sumOf {
+        it.first * it.second.flowRate
     }
 
-    private fun canSurpassMax(timeRemaining: Int, move: MoveState, currentMax: Int): Boolean {
-        return getTheoreticalMax(timeRemaining, move) > currentMax
-    }
-
-    private fun releasableValves(move: MoveState) =
-        (releasableValves - move.releasedValves.map { it.second }).sortedByDescending { it.flowRate }
+    private fun canSurpassMax(timeRemaining: Int, move: MoveState, currentMax: Int) =
+        getTheoreticalMax(timeRemaining, move) > currentMax
 
     private fun getTheoreticalMax(timeRemaining: Int, move: MoveState): Int {
         val currentScore = countTotalReleased(move)
-        val withElephant = move.elephantValve != null
+        val canReleaseNow =
+            canReleaseValve(move, move.myValve.currentValve) || (move.elephantValve != null && canReleaseValve(
+                move,
+                move.elephantValve.currentValve
+            ))
 
-        val releasable =
-            releasableValves(move).mapIndexed { ix, valve ->
-                maxOf(
-                    valve.flowRate * (timeRemaining - (if (!withElephant) 2 * ix else ix)),
-                    0
-                )
-            }
-                .sum()
+        val firstReleaseTime = if (canReleaseNow) timeRemaining else timeRemaining - 1
+        val optimisticReleaseTimes = (firstReleaseTime downTo 0 step 2)
 
-        return currentScore + releasable
+        val zipped = optimisticReleaseTimes zip releasableValves(move).sortedByDescending { it.flowRate }
+        return currentScore + zipped.sumOf { it.first * it.second.flowRate }
     }
+
+    private fun canReleaseValve(move: MoveState, valve: Valve) =
+        valve.flowRate > 0 && !move.releasedValves.any { it.second == valve }
+
+    private fun releasableValves(move: MoveState) =
+        (releasableValves - move.releasedValves.map { it.second })
 
     private fun runSimulation(startingState: MoveState, startingMoves: Int): Int {
         var movesRemaining = startingMoves
 
         var moves = listOf(startingState)
-        var currentMax: Int = 2500
+        var currentMax: Int = findAMax(startingState, startingMoves)
         println("Naive max: $currentMax")
 
         while (movesRemaining > 0 && moves.isNotEmpty()) {
             movesRemaining--
 
             moves = takeMoves(movesRemaining, moves)
-            val nonDistinct = moves.size
-            moves = moves.distinctBy { it.toDistinctHash() }
-
-            val movesPreFilter = moves.size
             val newMax = moves.maxOfOrNull(::countTotalReleased) ?: Int.MIN_VALUE
             currentMax = maxOf(currentMax, newMax)
-
-            println("$nonDistinct -> $movesPreFilter from distinct")
             moves = moves.filter { canSurpassMax(movesRemaining, it, currentMax) }
-            val movesPostFilter = moves.size
-            println("$movesPreFilter -> $movesPostFilter from max check")
-            val runtime = Runtime.getRuntime()
-            println("Actual attained so far: $newMax")
-            println("Current max: $currentMax")
-
-            println(runtime.freeMemory())
-            println(runtime.maxMemory())
-            println("")
+            println("$movesRemaining: ${moves.size} states, max score so far is $currentMax")
         }
 
         return currentMax
     }
 
     private fun MoveState.toDistinctHash() =
-        MoveHash(setOf(myValve, elephantValve), releasedValves.toSet())
+        MoveHash(setOf(myValve, elephantValve), releasedValves)
 
     private fun findAMax(initialState: MoveState, startingMoves: Int): Int {
         var movesRemaining = startingMoves
         var moves = listOf(initialState)
+        var currentMax = 0
 
-        while (movesRemaining > 0) {
+        while (movesRemaining > 0 && moves.isNotEmpty()) {
             movesRemaining--
             moves = takeMoves(movesRemaining, moves)
-            // println(moves.size)
-            if (movesRemaining % 5 == 0) {
-                moves = listOf(moves.maxBy { move -> countTotalReleased(move) })
+            currentMax = maxOf(currentMax, countTotalReleased(moves.first()))
+            if (moves.size > 1000) {
+                moves = moves.sortedByDescending { move -> countTotalReleased(move) }.subList(0, 1000)
             }
-            // println(move)
         }
 
-        return moves.maxOf(::countTotalReleased)
+        return currentMax
     }
 
     private fun takeMoves(currentTime: Int, currentMoves: List<MoveState>): List<MoveState> {
-        return currentMoves.flatMap { takeAllPossibleMoves(currentTime, it) }
+        return currentMoves.flatMap { takeAllPossibleMoves(currentTime, it) }.distinctBy { it.toDistinctHash() }
     }
 
     private fun releasedAllValves(move: MoveState) = move.releasedValves.size == releasableValves.size
@@ -128,8 +111,7 @@ class Day16 : Solver {
 
         val moves = mutableListOf<MoveState>()
 
-        val couldRelease = currentValve.flowRate > 0 && !move.releasedValves.any { it.second == currentValve }
-        if (couldRelease) {
+        if (canReleaseValve(move, currentValve)) {
             val newReleased = move.releasedValves.plus(Pair(currentTime, currentValve))
             val newPerson = PersonState(currentValve, emptySet())
             moves.add(move.copy(releasedValves = newReleased, myValve = newPerson))
@@ -154,9 +136,7 @@ class Day16 : Solver {
         val (currentValve, prevValves) = move.elephantValve ?: return listOf(move)
 
         val moves = mutableListOf<MoveState>()
-
-        val couldRelease = currentValve.flowRate > 0 && !move.releasedValves.any { it.second == currentValve }
-        if (couldRelease) {
+        if (canReleaseValve(move, currentValve)) {
             val newReleased = move.releasedValves.plus(Pair(currentTime, currentValve))
             val newPerson = PersonState(currentValve, emptySet())
             moves.add(move.copy(releasedValves = newReleased, elephantValve = newPerson))
