9995a972e5710cbc2827cd8bcb77670f42f97884
Alyssa <alyssa.burlton@gmail.com>
2022-12-17 07:16:56 +0000

    ffs

--- src/main/kotlin/17.kt
+++ src/main/kotlin/17.kt
@@ -1,10 +1,12 @@
-private data class Rock(val PointLs: List<PointL>, val type: RockType, val stopped: Boolean)
+private data class Rock(val points: List<PointL>, val type: RockType, val stopped: Boolean)
 private data class ChamberState(
-    var rocksDropped: Int,
+    var rocksDropped: Long,
     var windTicker: Int,
     var occupiedSpaces: MutableMap<Long, MutableList<PointL>>,
     val wind: List<Char>
 )
+
+private data class ChamberStateHash(val windTicker: Int, val normalisedHeights: String)
 
 private enum class RockType {
     HORIZONTAL_LINE,
@@ -25,7 +27,8 @@ class Day17 : Solver {
         val state = ChamberState(0, 0, mutableMapOf(), input)
 
         while (state.rocksDropped < 2022) {
-            val nextRockType = rockOrder[state.rocksDropped % (rockOrder.size)]
+            val nextRockType = rockOrder[(state.rocksDropped % (rockOrder.size)).toInt()]
+
             val rock = createRock(nextRockType, state.height())
             processTurn(rock, state)
             state.rocksDropped++
@@ -41,7 +44,7 @@ class Day17 : Solver {
             rock = state.dropRock(rock)
         }
 
-        val yValues = rock.PointLs.groupBy { it.y }
+        val yValues = rock.points.groupBy { it.y }
         yValues.forEach { (y, pts) ->
             val list = state.occupiedSpaces.getOrPut(y, ::mutableListOf)
             list.addAll(pts)
@@ -56,18 +59,51 @@ class Day17 : Solver {
     }
 
     override fun partB(): Any {
-        return ""
+        val state = ChamberState(0, 0, mutableMapOf(), input)
+        val hashesSeen = mutableMapOf<ChamberStateHash, Pair<Long, Long>>()
+        var heightFudgeFactor = 0L
+
+        while (state.rocksDropped < 1000000000000L) {
+            if (state.rocksDropped % 10000 == 0L) {
+                println("Done ${state.rocksDropped}")
+            }
+
+            val nextRockType = rockOrder[(state.rocksDropped % (rockOrder.size)).toInt()]
+            if (heightFudgeFactor == 0L && nextRockType == RockType.HORIZONTAL_LINE) {
+                val hash = state.toHash()
+                val new = !hashesSeen.containsKey(hash)
+                if (!new) {
+                    val (prevRocksThrown, prevHeight) = hashesSeen.getValue(hash)
+                    val heightGain = state.height() - prevHeight
+                    val cycleLength = state.rocksDropped - prevRocksThrown
+                    println("Found cycle from $prevRocksThrown -> ${state.rocksDropped} rocks. Height gain is $heightGain")
+                    val multiples = (1000000000000L / cycleLength)
+                    println("Setting fudge factor to $multiples * $heightGain")
+                    heightFudgeFactor = (multiples - 2) * heightGain
+                    state.rocksDropped += (multiples - 2) * cycleLength
+                    println("New rocksDropped is ${state.rocksDropped}")
+                } else {
+                    hashesSeen[hash] = Pair(state.rocksDropped, state.height())
+                }
+            }
+
+            val rock = createRock(nextRockType, state.height())
+            processTurn(rock, state)
+            state.rocksDropped++
+        }
+
+        return state.height() + heightFudgeFactor
     }
 
     private fun ChamberState.clearLines(restedRock: Rock) {
-        val yValues = restedRock.PointLs.map { it.y }.toSet()
-        val oldSize = occupiedSpaces.size
+        val yValues = restedRock.points.map { it.y }.toSet()
+        // val oldSize = occupiedSpaces.size
 
         val lowestLine = yValues.filter { occupiedSpaces[it]?.size == 7 }.minOfOrNull { it } ?: return
 
         occupiedSpaces = occupiedSpaces.filter { it.key >= lowestLine }.toMutableMap()
 
-        println("Cleared ${lowestLine+1} lines, $oldSize -> ${occupiedSpaces.size}")
+        // println("Cleared ${lowestLine+1} lines, $oldSize -> ${occupiedSpaces.size}")
     }
 
     private fun ChamberState.height() = occupiedSpaces.keys.maxOfOrNull { it + 1 } ?: 0L
@@ -78,7 +114,7 @@ class Day17 : Solver {
             return rock.copy(stopped = true)
         }
 
-        return rock.copy(PointLs = neighbours)
+        return rock.copy(points = neighbours)
     }
 
     private fun ChamberState.blowRock(rock: Rock): Rock {
@@ -92,15 +128,15 @@ class Day17 : Solver {
             return rock
         }
 
-        return rock.copy(PointLs = neighbours)
+        return rock.copy(points = neighbours)
     }
 
     private fun ChamberState.invalidLocation(pts: List<PointL>): Boolean =
         pts.any { it.x < 0 || it.x > 6 || it.y < 0 || (occupiedSpaces[it.y]?.contains(it) ?: false) }
 
-    private fun Rock.getDownNeighbours(): List<PointL> = PointLs.map { PointL(it.x, it.y - 1) }
-    private fun Rock.getLeftNeighbours(): List<PointL> = PointLs.map { PointL(it.x - 1, it.y) }
-    private fun Rock.getRightNeighbours(): List<PointL> = PointLs.map { PointL(it.x + 1, it.y) }
+    private fun Rock.getDownNeighbours(): List<PointL> = points.map { PointL(it.x, it.y - 1) }
+    private fun Rock.getLeftNeighbours(): List<PointL> = points.map { PointL(it.x - 1, it.y) }
+    private fun Rock.getRightNeighbours(): List<PointL> = points.map { PointL(it.x + 1, it.y) }
 
     private fun getPointLs(type: RockType): List<PointL> {
         return when(type) {
@@ -110,5 +146,15 @@ class Day17 : Solver {
             RockType.VERTICAL_LINE -> listOf(PointL(2, 0), PointL(2, 1), PointL(2, 2), PointL(2, 3))
             RockType.BOX -> listOf(PointL(2, 0), PointL(3, 0), PointL(2, 1), PointL(3, 1))
         }
+    }
+
+    private fun ChamberState.getNormalisedHeights(): List<Long> {
+        val xToHighestPoint: Map<Long, Long> = occupiedSpaces.values.flatten().groupBy { it.x }.mapValues { it.value.maxOf(PointL::y) }
+        val orderedHeights = xToHighestPoint.entries.sortedBy { it.key }.map { it.value }
+        return orderedHeights.map { it - orderedHeights.min() }
+    }
+
+    private fun ChamberState.toHash(): ChamberStateHash {
+        return ChamberStateHash(windTicker % wind.size, getNormalisedHeights().toString())
     }
 }
