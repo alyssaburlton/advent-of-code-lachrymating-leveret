1745c7438293831fd59b6ceddec03f16ef203955
Alyssa <alyssa.burlton@gmail.com>
2022-12-20 11:57:54 +0000

    immutable version, but now it's slow :(

--- src/main/kotlin/20.kt
+++ src/main/kotlin/20.kt
@@ -1,38 +1,30 @@
 class Day20 : Solver {
     override val day = 20
 
-
     private data class Number(val originalIx: Int, val value: Long)
 
     private val input = readIntegerList("20").map(Int::toLong).mapIndexed(::Number)
     private val decryptionKey = 811589153
 
-    override fun partA(): Any {
-        val newList = input.toMutableList()
-        mixList(newList)
-        return calculateGroveScore(newList)
-    }
+    override fun partA() = calculateGroveScore(mixList(input))
 
     override fun partB(): Any {
-        val newList = input.map { Number(it.originalIx, it.value * decryptionKey) }.toMutableList()
+        val newList = input.map { Number(it.originalIx, it.value * decryptionKey) }
 
-        repeat(10) {
-            mixList(newList)
-        }
-
-        return calculateGroveScore(newList)
+        return (1..10).fold(newList) { list, _ ->
+            mixList(list)
+        }.let(::calculateGroveScore)
     }
 
-    private fun mixList(list: MutableList<Number>) {
-        list.indices.forEach { originalIx ->
-            val element = list.first { it.originalIx == originalIx }
-            val currentIx = list.indexOf(element)
+    private fun mixList(list: List<Number>) =
+        list.indices.fold(list) { currentList, originalIx ->
+            val element = currentList.first { it.originalIx == originalIx }
+            val currentIx = currentList.indexOf(element)
             val newIx = (currentIx + element.value).mod(input.size - 1)
 
-            list.removeAt(currentIx)
-            list.add(newIx, element)
+            val without = currentList.minus(element)
+            without.subList(0, newIx) + element + without.subList(newIx, without.size)
         }
-    }
 
     private fun calculateGroveScore(decryptionResult: List<Number>): Long {
         val decryptedFile = decryptionResult.map { it.value }
