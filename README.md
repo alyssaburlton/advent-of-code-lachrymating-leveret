9731a299cd7719ae28a6e8cda1b314784839726b
Alyssa <alyssa.burlton@gmail.com>
2022-12-25 10:28:23 +0000

    rough code version of what I did with pen and paper, but it doesn't work for all numbers (e.g. 12)

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # aoc-lachrymating-leveret
 
-2022 Advent of Code solutions, in Kotlin.
+Kotlin solutions to Advent of Code 2022 (also known as
+the [year of the lachrymating leveret](https://twitter.com/terryandrob/status/1466043256391753733))
 
 ## Goals
 
 As I'm not stretching myself with a new language this year, I've set myself some other challenges instead:
 
-- ⌛ Try to finish in the top 10 of
+- ✔ Try to finish in the top 10 of
   the [Kotlin leaderboard](https://adventofcode.com/2022/leaderboard/private/view/236080)
+    - I came **5th**! 🎉
 - ✔ Try to make the top 100 globally just once, for any part of any problem
     - ✔ **85th** for [Day 8B](https://adventofcode.com/2022/leaderboard/day/8)
     - 🤏 **103rd** for Day 24A
--- src/main/kotlin/25.kt
+++ src/main/kotlin/25.kt
@@ -1,44 +1,63 @@
+import kotlin.math.abs
 import kotlin.math.pow
+import kotlin.math.sign
 
 class Day25 : Solver {
     override val day = 25
 
-    private val input = readStringList("25").map(::snafuToDecimal)
-    private val snafuMap: Map<String, Int> = mapOf(
-        "2" to 2,
-        "1" to 1,
-        "0" to 0,
-        "-" to -1,
-        "=" to -2
+    private val input = readStringList("25")
+    private val snafuMap: Map<Char, Int> = mapOf(
+        '2' to 2,
+        '1' to 1,
+        '0' to 0,
+        '-' to -1,
+        '=' to -2
     )
 
-    override fun partA(): Any {
-        println(snafuToDecimal("2=-0=01----22-0-1-10"))
-        return input.sum()
-    }
+    override fun partA() = input.sumOf(::snafuToDecimal).let(::decimalToSnafu)
 
-    override fun partB(): Any {
-        return ""
-    }
+    override fun partB() = "Merry Christmas!"
 
-    private fun snafuToDecimal(snafu: String): Long {
-        val length = snafu.length
-        val characters = snafu.toCharArray().toList()
-
-        val snafuMap: Map<String, Int> = mapOf(
-            "2" to 2,
-            "1" to 1,
-            "0" to 0,
-            "-" to -1,
-            "=" to -2
-        )
-
-        // println(snafuMap)
-
-        return characters.mapIndexed { ix, char ->
-            val power = length - ix - 1
-            // println(char)
-            snafuMap.getValue(char.toString()).toDouble() * 5.0.pow(power)
+    fun snafuToDecimal(snafu: String) =
+        snafu.toCharArray().mapIndexed { ix, char ->
+            val power = snafu.length - ix - 1
+            snafuMap.getValue(char) * 5.0.pow(power)
         }.sum().toLong()
+
+    fun decimalToSnafu(number: Long): String {
+        val map = decimalToSnafuRecursive(-number, emptyMap())
+        val maxPower = map.keys.max()
+
+        return (maxPower downTo 0).joinToString("") {
+            val count = map[it] ?: 0
+            if (count == -2) "=" else if (count == -1) "-" else count.toString()
+        }
     }
+
+    private tailrec fun decimalToSnafuRecursive(amountRemaining: Long, powersSoFar: Map<Int, Int>): Map<Int, Int> {
+        if (amountRemaining == 0L) {
+            return powersSoFar
+        }
+
+        val minPowerSoFar = powersSoFar.keys.minOrNull() ?: 100
+
+        val power = findRelevantPowerOfFive(amountRemaining, minPowerSoFar)
+
+        val diff2 = (2 * 5.0.pow(power)) - abs(amountRemaining)
+        val diff = 5.0.pow(power) - abs(amountRemaining)
+        val amountToUse = if (abs(diff2) < abs(diff)) 2 else 1
+
+        val sign = amountRemaining.sign
+        val newPair = power to (-sign) * amountToUse
+
+        return decimalToSnafuRecursive(
+            amountRemaining + ((-sign) * amountToUse * 5.0.pow(power)).toLong(),
+            powersSoFar + newPair
+        )
+    }
+
+    private fun findRelevantPowerOfFive(amountRemaining: Long, maxPower: Int = 100) =
+        (0 until maxPower).firstOrNull { n ->
+            2 * 5.0.pow(n) >= abs(amountRemaining)
+        } ?: (maxPower - 1)
 }
